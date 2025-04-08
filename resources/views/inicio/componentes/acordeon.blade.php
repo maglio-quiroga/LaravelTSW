95622d0e5444b33aec80c618d2cea740e97ae60b
Maglio <magliocquirogaf@gmail.com>
2025-04-07 20:31:59 -0400

    dinamico en el inicio listo

--- resources/views/inicio/componentes/acordeon.blade.php
+++ resources/views/inicio/componentes/acordeon.blade.php
@@ -2,71 +2,22 @@
 <div class="container">
       <h2 style="color:#416788;">ACLAREMOS TUS DUDAS</h2>
       <div class="faqs">
+        @foreach ($faqs as $faq)
         <div class="faq">
           <div class="head">
-            <span>¿Hacen envíos a todo el mundo?</span>
+            <span>{{ $faq->titulo }}</span>
             <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M9 14.469L1 6.46897L1.96897 5.5L9 12.531L16.031 5.5L17 6.46897L9 14.469Z" fill="black" />
             </svg>
           </div>
           <div class="content">
             <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-              consectetur totam saepe id quae, voluptate, obcaecati temporibus
-              alias unde dolore voluptatem aperiam placeat possimus! Labore est
-              repudiandae sequi veniam aliquid?
+            {{ $faq->texto }}
             </p>
           </div>
-        </div>
-        <div class="faq">
-          <div class="head">
-            <span>
-              ¿En cuantos días llega mi pedido?
-            </span>
-            <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M9 14.469L1 6.46897L1.96897 5.5L9 12.531L16.031 5.5L17 6.46897L9 14.469Z" fill="black" />
-            </svg>
-          </div>
-          <div class="content">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-              consectetur totam saepe id quae, voluptate, obcaecati temporibus
-              alias unde dolore voluptatem aperiam placeat possimus! Labore est
-              repudiandae sequi veniam aliquid?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-              consectetur totam saepe id quae, voluptate, obcaecati temporibus
-              alias unde dolore voluptatem aperiam placeat possimus! Labore est
-              repudiandae sequi veniam aliquid?
-            </p>
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-              consectetur totam saepe id quae, voluptate, obcaecati temporibus
-              alias unde dolore voluptatem aperiam placeat possimus! Labore est
-              repudiandae sequi veniam aliquid?
-            </p>
-          </div>
-        </div>
-        <div class="faq">
-          <div class="head">
-            <span>
-              ¿Cómo se manejan las devoluciones?
-            </span>
-            <svg width="18" height="19" viewBox="0 0 18 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-              <path d="M9 14.469L1 6.46897L1.96897 5.5L9 12.531L16.031 5.5L17 6.46897L9 14.469Z" fill="black" />
-            </svg>
-          </div>
-          <div class="content">
-            <p>
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit
-              consectetur totam saepe id quae, voluptate, obcaecati temporibus
-              alias unde dolore voluptatem aperiam placeat possimus! Labore est
-              repudiandae sequi veniam aliquid?
-            </p>
-          </div>
-        </div>
       </div>
+      @endforeach
+</div>
 </div>
 
 <script src="{{ asset('js/inicio/acordeon.js') }}"></script>
